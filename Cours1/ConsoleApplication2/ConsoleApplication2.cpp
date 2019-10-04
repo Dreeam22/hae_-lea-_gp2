5f39290cabf51bae0384a93b1521dc357723c05b
Lea Frayssinhes <frayssinhes.l31@gmail.com>
2019-10-04 10:28:58 +0200

    cours 1 exo

---
+++ Cours1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,70 @@
+// ConsoleApplication2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+void Memcpy(char * dest, char * src, int size) {
+
+	/*for (int i=0; i < size; i++)
+	{
+		dest[i] = src[i];
+	}*/
+	
+	while (size--)
+	{
+		dest[size] = src[size];
+	}
+}
+
+
+int StrChr(char* grange, char chat)
+{
+	int length = strlen(grange);
+	for (int i = 0; i < length ; i++)
+	{
+		if (grange[i] == chat)
+		{
+			return i;
+		}	
+		
+	}
+	return -1;
+
+	/*int i = 0;
+	while (strlen(grange))
+	{
+		if (grange[i] == chat)
+		{
+			return i + 1;
+		}
+
+		if (i == strlen(grange))
+		{
+			return -1;
+		}
+	}*/
+	/*trouver la position du chat
+		dans la grange
+		si pas trouvé
+		renvoyer -1*/
+}
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	char licorne[32] = "licorne";
+	char vomi[32] = "vomit";
+	Memcpy (licorne, vomi, strlen(licorne)+1);
+
+	printf("licorne %s\n", licorne);
+
+	char chateau[32] = "chateau";
+	char soldat = 'e';
+
+	printf("Le soldat est à la position %d\n", StrChr(chateau, soldat));
+}
+
+
